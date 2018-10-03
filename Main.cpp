ff7c2951a09223ba5eeadabe39af6bb8f425e2bb
p <cabalor@gmail.com>
2018-10-03 15:20:02 +0200

    first programs

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+
+
+
+int main() {
+
+	double waga, wzrost, bmi;
+
+
+	std::cout << "xx " << std::endl;
+
+	std::cout << "podaj wage" << std::endl;
+
+	std::cin >> waga;
+
+	std::cout << "podaj wzrost" << std::endl;
+
+	std::cin >> wzrost;
+	if (wzrost > 1) {
+		wzrost /= 100;
+	}
+
+	bmi = waga / (wzrost * wzrost);
+
+
+	std::cout << bmi << std::endl;
+
+
+	system("pause");
+
+}
---
+++ Source.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+
+#define GETMAX
+#define GETMIN
+
+
+
+int main() {
+
+	int jeden, dwa, trzy;
+
+	std::cin >> jeden >> dwa >> trzy;
+
+	std::cout << "wyniki " << jeden << dwa << trzy << std::endl;
+
+
+#if defined(GETMAX)
+
+	std::cout << "najwieksza liczba" << std::endl;
+
+	int mx = jeden;
+	if (dwa > mx) mx = dwa;
+	if (trzy > mx) mx = trzy;
+
+
+		std::cout << "najwieksza liczba jest pierwsz liczba "<< mx << std::endl;
+
+
+#endif
+
+#if defined(GETMIN)
+
+		std::cout << "najwieksza liczba" << std::endl;
+
+		int min = jeden;
+		if (dwa < min) min = dwa;
+		if (trzy < min) min = trzy;
+
+
+		std::cout << "najmniejsza liczba jest pierwsz liczba " << min << std::endl;
+
+
+#endif
+
+
+		std::cout << "------------ zadanie 3-----------------" << std::endl;
+
+
+		int j, d, t;
+
+		std::cin >> j >> d >> t;
+
+		std::cout << "wyniki " << j << d << t << std::endl;
+
+		int loop = j;
+		if (d > loop) {
+			loop = d;
+		}
+		if (t > loop) {
+			loop = t;
+		}
+
+
+		for (int i = loop; i > 0; i--) {
+			if (i <= j) {
+				std::cout << "*";
+			}
+			else {
+				std::cout << " ";
+			}
+			if (i <= d) {
+				std::cout << "*";
+			}
+			else {
+				std::cout << " ";
+			}
+			if (i <= t) {
+				std::cout << "*";
+			}
+			else {
+				std::cout << " ";
+			}
+			std::cout <<std::endl;
+		}
+
+
+
+	system("pause");
+
+
+}
+
+
+
+
+
